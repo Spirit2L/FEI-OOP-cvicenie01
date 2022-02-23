0173855580b881afdda61c205f3343aeeae4ac89
Spirit2L <jakub.putala@gmail.com>
2022-02-23 22:22:25 +0100

    funkcia + pole

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,11 +1,17 @@
 package sk.stuba.fei.uim.oop;
 
-public class App {
-    public static void main(String[] args) {
-        int cislo = Integer.parseInt(args[0]);
-        if (cislo == 0)
-            System.out.println("je to nula");
-        else
-            System.out.println("nie je to nula");
+public class App{
+    public static void main(String[] args){
+        int[] pole = new int[10];
+        for (int i = 0; i < pole.length; i++){
+            pole[i] = i;
+        }
+        for (int i = 0; i <pole.length; i++){
+            vypis(i);
+        }
+
+    }
+    public static void vypis(int a){
+        System.out.println(a);
     }
 }
