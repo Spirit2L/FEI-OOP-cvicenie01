e99e244ef3780c01fd05aecb072c07b9bf8959ae
Spirit2L <jakub.putala@gmail.com>
2022-02-23 22:12:30 +0100

    if

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,19 +2,10 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int celeCislo = 5;
-        long celeVacsieCislo = 5L;
-        float desatinneCislo = 5.5f;
-        double dlhsieDesatinneCislo = 5.5;
-        char znak = 'a';
-        String retazec = "retazec";
-        boolean bool = false;
-
-        int podiel = 2/3;
-        System.out.println(podiel);
-        int zvysok = 2%3;
-        System.out.println(zvysok);
-        float podielFloat = 2/3f;
-        System.out.println(podielFloat);
+        int cislo = Integer.parseInt(args[0]);
+        if (cislo == 0)
+            System.out.println("je to nula");
+        else
+            System.out.println("nie je to nula");
     }
 }
